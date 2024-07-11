a5c231c09d2b3f34badd5b83a1f0d62f66c3f065
Dmitry Blinov <dmitry@orderly.network>
2024-07-11 15:54:16 +0300

    Add cefi_mock express mock server

---
+++ test-page/cefi_mock/cefi_mock.ts
@@ -0,0 +1,32 @@
+import express from 'express';
+
+const app = express();
+const port = process.env.PORT || 3001; // Use a different port from Next.js
+
+app.get('/api', (req, res) => {
+    res.json({ message: 'Hello from Express!' });
+});
+
+app.listen(port, () => {
+    console.log(`Express server listening on port ${port}`);
+});
+
+app.get('/v1/registration_nonce', (req, res) => {
+    res.json({
+        "success": true,
+        "data": {
+            "registration_nonce": "119747133480"
+        },
+        "timestamp": 1689074684188
+    });
+});
+
+app.get('/v1/withdraw_nonce', (req, res) => {
+    res.json({
+        "success": true,
+        "data": {
+            "withdraw_nonce": 14001212121234
+        },
+        "timestamp": 1685673968302
+    });
+});
