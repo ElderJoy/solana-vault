e3e0e1febc44829a3f1a040ab0c7ab408c3fdd37
Dmitry Blinov <dmitry@orderly.network>
2024-07-24 20:42:19 +0300

    First implementation of deposit: token transferred to admin account

--- vault/programs/vault/src/lib.rs
+++ vault/programs/vault/src/lib.rs
@@ -1,4 +1,6 @@
 use anchor_lang::prelude::*;
+use anchor_spl::token::{self, Transfer};
+use anchor_spl::token_interface::{Mint, TokenAccount, TokenInterface};
 
 declare_id!("7u49DKPmZd3M1PtpNishUTjM6EV2yU72AaqN1RSiZBXs");
 
@@ -6,11 +8,50 @@ declare_id!("7u49DKPmZd3M1PtpNishUTjM6EV2yU72AaqN1RSiZBXs");
 pub mod vault {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn deposit(ctx: Context<Deposit>, amount: u64) -> Result<()> {
+        msg!("Instruction: Deposit");
+
+        let cpi_accounts = Transfer {
+            from: ctx.accounts.user_deposit_wallet.to_account_info(),
+            to: ctx.accounts.admin_deposit_wallet.to_account_info(),
+            authority: ctx.accounts.user.to_account_info(),
+        };
+        let cpi_program = ctx.accounts.token_program.to_account_info();
+        let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
+        token::transfer(cpi_ctx, amount)?;
+
+        let user_info = &mut ctx.accounts.user_info;
+        user_info.authority = ctx.accounts.user.key();
+        user_info.amount = amount;
+
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Deposit<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+    #[account(mut)]
+    pub admin: AccountInfo<'info>,
+    #[account(init, payer = user, space = 8 + UserInfo::LEN)]
+    pub user_info: Account<'info, UserInfo>,
+    #[account(mut)]
+    pub user_deposit_wallet: InterfaceAccount<'info, TokenAccount>,
+    #[account(mut)]
+    pub admin_deposit_wallet: InterfaceAccount<'info, TokenAccount>,
+    #[account(mut)]
+    pub deposit_token: InterfaceAccount<'info, Mint>,
+    pub token_program: Interface<'info, TokenInterface>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct UserInfo {
+    pub authority: Pubkey,
+    pub amount: u64,
+}
+
+impl UserInfo {
+    pub const LEN: usize = 32 + 8;
+}
--- vault/tests/vault.ts
+++ vault/tests/vault.ts
@@ -1,16 +1,82 @@
+import { join } from "path";
+import { readFileSync } from "fs";
 import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
+import { BN, Program } from "@coral-xyz/anchor";
 import { Vault } from "../target/types/vault";
+import {
+  Keypair,
+  LAMPORTS_PER_SOL,
+  PublicKey,
+  SystemProgram,
+} from "@solana/web3.js";
+import { TOKEN_PROGRAM_ID, Token } from "@solana/spl-token";
+import { assert } from "chai";
 
 describe("vault", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.Vault as Program<Vault>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  const WALLET_PATH = join(process.env["HOME"]!, ".config/solana/id.json");
+  const admin = Keypair.fromSecretKey(
+    Buffer.from(JSON.parse(readFileSync(WALLET_PATH, { encoding: "utf-8" })))
+  );
+
+  const user = Keypair.generate();
+  const userInfo = Keypair.generate();
+
+  let token: Token;
+  let adminTokenAccount: PublicKey;
+  let userTokenAccount: PublicKey;
+
+  before(async () => {
+    await provider.connection.confirmTransaction(
+      await provider.connection.requestAirdrop(
+        user.publicKey,
+        10 * LAMPORTS_PER_SOL
+      ),
+      "confirmed"
+    );
+
+    token = await Token.createMint(
+      provider.connection,
+      admin,
+      admin.publicKey,
+      null,
+      9,
+      TOKEN_PROGRAM_ID
+    );
+
+    adminTokenAccount = await token.createAccount(admin.publicKey);
+    userTokenAccount = await token.createAccount(user.publicKey);
+
+    await token.mintTo(userTokenAccount, admin.publicKey, [admin], 1e10);
+  });
+
+  it("deposit", async () => {
+    const userTokenAccountBefore = await token.getAccountInfo(userTokenAccount);
+    assert.strictEqual(userTokenAccountBefore.amount.toNumber(), 1e10);
+
+    const tx = await program.methods.deposit(new BN(1e10)).accounts({
+      user: user.publicKey,
+      admin: admin.publicKey,
+      userInfo: userInfo.publicKey,
+      userDepositWallet: userTokenAccount,
+      adminDepositWallet: adminTokenAccount,
+      depositToken: token.publicKey,
+      tokenProgram: TOKEN_PROGRAM_ID,
+    }).signers([user, userInfo]).rpc();
+    console.log("Deposit transaction signature", tx);
+
+    const adminTokenAccountAfter = await token.getAccountInfo(adminTokenAccount);
+    assert.strictEqual(adminTokenAccountAfter.amount.toNumber(), 1e10);
+
+    const userTokenAccountAfter = await token.getAccountInfo(userTokenAccount);
+    assert.strictEqual(userTokenAccountAfter.amount.toNumber(), 0);
+
+    const userInfoAfter = await program.account.userInfo.fetch(userInfo.publicKey);
+    assert.strictEqual(userInfoAfter.amount.toNumber(), 1e10);
   });
 });
